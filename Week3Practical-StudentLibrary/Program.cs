d2e1c2c371fcb5fff972a069d8fccc787173ee71
aaronconveryUU <convery-a8@ulster.ac.uk>
2025-10-15 16:27:33 +0100

    Challenge 1 Complete

--- Week3Practical-StudentLibrary/Program.cs
+++ Week3Practical-StudentLibrary/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Please Work!");
+﻿using Week3Practical_StudentLibrary;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Please Work");
+        Student student1 = new Student();
+        Student student2 = new Student("John Doe", 18);
+        student1.Display();
+        Console.WriteLine(student1.GetOlder());
+        student2.Display();
+        Console.WriteLine(student2.GetOlder());
+    }
+}
---
+++ Week3Practical-StudentLibrary/Student.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week3Practical_StudentLibrary
+{
+    internal class Student
+    {
+        private int id;
+        private string name;
+        private int age;
+        private static int studentCount = 0;
+
+        public int Id
+        {
+            get { return id; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        public static int StudentCount
+        {
+            get { return studentCount; }
+        }
+
+        public Student()
+        {
+            Name = "John Doe";
+            Age = 16;
+            id = studentCount++;
+        }
+
+        public Student(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+            id = studentCount++;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"{id}\n{Name}\n{Age}");
+        }
+
+        public int GetOlder()
+        {
+            return Age += 1;
+        }
+    }
+}
